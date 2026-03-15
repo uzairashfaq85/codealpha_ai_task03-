4e1d6d9f25a516921d3d9818968af3e6a3039c94
uzaireeyy <uzairashfaq85@gmail.com>
2026-03-15 19:31:51 +0100

    refactor: move separate-model trainer to scripts

--- scripts/rnn_separate.py
+++ scripts/rnn_separate.py
@@ -1,23 +1,35 @@
-import os, sys
+"""
+Project: codealpha_ai_task03-
+Created: August 2024
+Description: Train baseline melody-only or harmony-only softmax model variants.
+"""
+
 import argparse
-import time
 import itertools
-import cPickle
 import logging
+import os
+import pickle
 import random
 import string
-import pprint
+import sys
+import time
  
 import numpy as np
-import tensorflow as tf    
+import tensorflow.compat.v1 as tf
 import matplotlib.pyplot as plt
 
-import midi_util
-import nottingham_util
-import sampling
-import util
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(CURRENT_DIR)
+SRC_DIR = os.path.join(PROJECT_ROOT, "src")
+if SRC_DIR not in sys.path:
+    sys.path.insert(0, SRC_DIR)
+
+from music_rnn import nottingham_util
+from music_rnn import util
 from rnn import get_config_name, DefaultConfig
-from model import Model, NottinghamSeparate
+from music_rnn.model import NottinghamSeparate
+
+tf.disable_v2_behavior()
 
 if __name__ == '__main__':
     np.random.seed()      
@@ -29,24 +41,30 @@ if __name__ == '__main__':
                         choices = ['bach', 'nottingham', 'softmax'])
     parser.add_argument('--model_dir', type=str, default='models')
     parser.add_argument('--run_name', type=str, default=time.strftime("%m%d_%H%M"))
+    parser.add_argument('--pickle_path', type=str, default=nottingham_util.PICKLE_LOC)
+    parser.add_argument('--num_epochs', type=int, default=None)
+    parser.add_argument('--max_time_batches', type=int, default=None)
+    parser.add_argument('--time_batch_len', type=int, default=None)
+    parser.add_argument('--fast_dev_run', action='store_true', default=False)
 
     args = parser.parse_args()
 
     if args.dataset == 'softmax':
-        resolution = 480
         time_step = 120
         model_class = NottinghamSeparate
-        with open(nottingham_util.PICKLE_LOC, 'r') as f:
-            pickle = cPickle.load(f)
-            chord_to_idx = pickle['chord_to_idx']
+        if not os.path.exists(args.pickle_path):
+            raise FileNotFoundError(
+                "Dataset pickle not found: {}. Run scripts/create_demo_data.py or scripts/main.py first.".format(
+                    args.pickle_path
+                )
+            )
+        with open(args.pickle_path, 'rb') as f:
+            data_pickle = pickle.load(f)
 
-        input_dim = pickle["train"][0].shape[1]
-        print 'Finished loading data, input dim: {}'.format(input_dim)
+        input_dim = data_pickle["train"][0].shape[1]
+        print('Finished loading data, input dim: {}'.format(input_dim))
     else:
         raise Exception("Other datasets not yet implemented")
-
-
-    initializer = tf.random_uniform_initializer(-0.1, 0.1)
 
     best_config = None
     best_valid_loss = None
@@ -54,7 +72,7 @@ if __name__ == '__main__':
     # set up run dir
     run_folder = os.path.join(args.model_dir, args.run_name)
     if os.path.exists(run_folder):
-        raise Exception("Run name {} already exists, choose a different one", format(run_folder))
+        raise Exception("Run name {} already exists, choose a different one".format(run_folder))
     os.makedirs(run_folder)
 
     logger = logging.getLogger(__name__) 
@@ -71,7 +89,8 @@ if __name__ == '__main__':
     }
 
     # Generate product of hyperparams
-    runs = list(list(itertools.izip(grid, x)) for x in itertools.product(*grid.itervalues()))
+    grid_keys = list(grid.keys())
+    runs = [list(zip(grid_keys, x)) for x in itertools.product(*grid.values())]
     logger.info("{} runs detected".format(len(runs)))
 
     for combination in runs:
@@ -82,8 +101,20 @@ if __name__ == '__main__':
         for attr, value in combination:
             setattr(config, attr, value)
 
+        if args.fast_dev_run:
+            config.num_epochs = 2
+            config.max_time_batches = -1
+            config.time_batch_len = 32
+            config.hidden_size = 64
+        if args.num_epochs is not None:
+            config.num_epochs = args.num_epochs
+        if args.max_time_batches is not None:
+            config.max_time_batches = args.max_time_batches
+        if args.time_batch_len is not None:
+            config.time_batch_len = args.time_batch_len
+
         if config.dataset == 'softmax':
-            data = util.load_data('', time_step, config.time_batch_len, config.max_time_batches, nottingham=pickle)
+            data = util.load_data('', time_step, config.time_batch_len, config.max_time_batches, nottingham=data_pickle)
             config.input_dim = data["input_dim"]
         else:
             raise Exception("Other datasets not yet implemented")
@@ -91,7 +122,7 @@ if __name__ == '__main__':
         # cut away unnecessary parts
         r = nottingham_util.NOTTINGHAM_MELODY_RANGE
         if args.choice == 'melody':
-            print "Using only melody"
+            print("Using only melody")
             for d in ['train', 'test', 'valid']:
                 new_data = []
                 for batch_data, batch_targets in data[d]["data"]:
@@ -99,7 +130,7 @@ if __name__ == '__main__':
                                      [tb[:, :, 0] for tb in batch_targets]))
                 data[d]["data"] = new_data
         else:
-            print "Using only harmony"
+            print("Using only harmony")
             for d in ['train', 'test', 'valid']:
                 new_data = []
                 for batch_data, batch_targets in data[d]["data"]:
@@ -109,12 +140,12 @@ if __name__ == '__main__':
 
         input_dim = data["input_dim"] = data["train"]["data"][0][0][0].shape[2]
         config.input_dim = input_dim
-        print "New input dim: {}".format(input_dim)
+        print("New input dim: {}".format(input_dim))
 
         logger.info(config)
         config_file_path = os.path.join(run_folder, get_config_name(config) + '.config')
-        with open(config_file_path, 'w') as f: 
-            cPickle.dump(config, f)
+        with open(config_file_path, 'wb') as f:
+            pickle.dump(config, f, protocol=pickle.HIGHEST_PROTOCOL)
 
         with tf.Graph().as_default(), tf.Session() as session:
             with tf.variable_scope("model", reuse=None):
@@ -122,8 +153,8 @@ if __name__ == '__main__':
             with tf.variable_scope("model", reuse=True):
                 valid_model = model_class(config, training=False)
 
-            saver = tf.train.Saver(tf.all_variables())
-            tf.initialize_all_variables().run()
+            saver = tf.train.Saver(tf.global_variables())
+            tf.global_variables_initializer().run()
 
             # training
             early_stop_best_loss = None
